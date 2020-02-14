6a5a379a536541879cefb64c77bbe7e984053b2f
Nathanael Roskens <natezxc@gmail.com>
2020-02-14 14:30:08 -0600

    Updated Statistics

--- Statistics.java
+++ Statistics.java
@@ -6,33 +6,39 @@ public class Statistics {
     static int win;
     static int lose;
     static int gameCount;
+
 //    Game A = new Game();
     Statistics () {
         this.win = 0;
         this.lose = 0;
         this.gameCount = 0;
+
     }
 
     public static int gamesPlayed (Game A) {
-        gameCount = A.gamePlayed();
+        gameCount = gameCount+A.gamePlayed();
         System.out.println("Game count from Stats: " + gameCount);
         return gameCount;
     }
 
     public static int gamesWon (Game A) {
-        win = A.playerWin();
+        win = win + A.playerWin();
         System.out.println("Game(s) won from stats: " + win);
+
         return win;
     }
 
 
     public int gamesLost (Game A) {
-        lose = A.playerLose();
+        lose = lose + A.playerLose();
         System.out.println("Game(s) lost from stats: " + lose);
+
         return lose;
   }
-//
-//    public void percentages () {
-//        System.out.println("Your win percentage is: "+ win/gameCount);
-//    }
+
+
+    public void percentages (Game A) {
+        float percentages = (int)((float)win/(float)gameCount * 100);
+        System.out.println("Your win percentage is: "+ percentages+ "%");
+    }
 }
